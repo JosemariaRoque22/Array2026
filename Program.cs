90a4684d25a78ab9e0be61fa6eb80f1e3bf0c98e
Josemaria Roque <N00502140@upn.pe>
2026-09-23 12:37:40 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+ static void ImprimirNotas(int[] notasx)
+{
+ for(int i = 0; i < notasx.Length; i++)
+    {
+    Console.WriteLine($"Nota {i + 1}: {notasx[i]}");
+    }
+}
+
+int[] notas = {15,18,12,17,20};
+
+ImprimirNotas(notas);
+
